bf420d4def7aaf2745d8d90824a8cab87c30f9fc
Jaesung <wotjd4725@gmail.com>
2023-04-19 23:52:23 +0900

    algo TIL

--- dp/G1_2213_트리의독립집합.java
+++ dp/G1_2213_트리의독립집합.java
@@ -1,9 +1,10 @@
 package dp;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class G1_2213_트리의독립집합 {
@@ -21,7 +22,8 @@ public class G1_2213_트리의독립집합 {
 	static boolean[][] map;
 	static boolean[] visited;
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		FileInputStream fis = new FileInputStream("src/input.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N+1];
 		map = new boolean[N+1][N+1];
@@ -30,14 +32,36 @@ public class G1_2213_트리의독립집합 {
 		for (int n = 1; n <= N; n++) {
 			arr[n] = Integer.parseInt(st.nextToken());
 		}
-		while ((st = new StringTokenizer(br.readLine())).hasMoreTokens()) {
+		String input = "";
+		while ((input = br.readLine()) != null) {
+			st = new StringTokenizer(input);
 			int v1 = Integer.parseInt(st.nextToken());
 			int v2 = Integer.parseInt(st.nextToken());
 			map[v1][v2] = true;
 			map[v2][v1] = true;
 		}
-		System.out.println(3);
-		dp(1);
+		Node[] ret = dp(1);
+		int answer = 0;
+		String[] answerRoute;
+		String route = "";
+		if (ret[0].value >= ret[1].value) {
+			answer = ret[0].value;
+			route = ret[0].path.toString();
+		}
+		else {
+			answer = ret[1].value; 
+			route = ret[1].path.toString();
+		}
+		answerRoute = route.split(" ");
+		Arrays.sort(answerRoute);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < answerRoute.length; i++) {
+			System.out.println(answerRoute[i]);
+			sb.append(answerRoute[i] + " ");
+		}
+		System.out.println(answer);
+		System.out.println(sb);
+		
 	}
 	
 	static Node[] dp(int now) {
@@ -49,13 +73,19 @@ public class G1_2213_트리의독립집합 {
 		for (int n = 2; n <= N; n++) {
 			if (!map[now][n] || visited[n]) continue;
 			Node[] ret = dp(n);
-			useValue += ret[0].value;
+			if (ret[0].value > ret[1].value) {
+				useValue += ret[0].value;
+				useSb.append(" " + ret[0].path);
+			}
+			else {
+				useValue += ret[1].value;
+				useSb.append(" " + ret[1].path);
+			}
 			notUseValue += ret[1].value;
-			useSb.append(ret[0].path);
-			notUseSb.append(ret[1].path);
+			notUseSb.append(" " + ret[1].path);
 			
 		}
-		Node useMe = new Node(notUseValue + arr[now], notUseSb.append(now));
+		Node useMe = new Node(notUseValue + arr[now], notUseSb.append(" " + now));
 		Node notUseMe;
 		if (useValue >= notUseValue) {
 			notUseMe = new Node(useValue, useSb);
@@ -63,6 +93,7 @@ public class G1_2213_트리의독립집합 {
 		else {
 			notUseMe = new Node(notUseValue, notUseSb);
 		}
+
 		return new Node[] {useMe, notUseMe};
 		
 	}
