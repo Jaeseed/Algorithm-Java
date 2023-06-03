1f670837a9d2513733310913b112b30f7fc17ec2
Jaesung <wotjd4725@gmail.com>
2023-06-03 17:01:25 +0900

    algo TIL

--- tree/G1_2263_트리의순회.java
+++ tree/G1_2263_트리의순회.java
@@ -6,12 +6,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class G1_2263_트리의순회 {
-	static int inIdx;
-	static int postIdx;
-	static boolean[] visited;
+	static int[] pre;
+	static int idx;
+	static int N;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		
 		// 0 행은 in order 1행은 post order
 		int[][] orders = new int[2][N];
@@ -22,49 +22,25 @@ public class G1_2263_트리의순회 {
 			}
 		}
 		
-		visited = new boolean[N+1];
-		inIdx = N - 1;
-		postIdx = N - 1;
-		visited[orders[1][N-1]] = true;
-		Node root = new Node(orders[1][N-1]);
-		order(root, orders[0], orders[1]);
-		System.out.println(3);
-	}
-	
-	static void order(Node now, int[] inOrder, int[] postOrder) {
-		if (inOrder[inIdx] != now.num) {
-			postIdx--;
-			int rightNum = postOrder[postIdx];
-			Node right = new Node(rightNum);
-			visited[rightNum] = true;
-			now.setRight(right);
-			order(right, inOrder, postOrder);
-		}
-		inIdx--;
-		if (visited[inOrder[inIdx]]) return;
-		int leftNum = inOrder[postIdx];
-		Node left = new Node(leftNum);
-		visited[leftNum] = true;
-		order(left, inOrder, postOrder);
-
-	}
-	
-	static class Node {
-		int num;
-		Node left;
-		Node right;
+		pre = new int[N];
 		
-		Node (int num) {
-			this.num = num;
+		tree(0, N - 1, 0, N - 1, orders[0], orders[1]);
+		StringBuilder sb = new StringBuilder();
+		for (int n = 0; n < N; n++) {
+			sb.append(pre[n] + " ");
 		}
+		System.out.println(sb);
 		
-		public void setLeft(Node left) {
-			this.left = left;
-		}
-		
-		public void setRight(Node right) {
-			this.right = right;
-		}
 	}
 
+	static void tree(int is, int ie, int ps, int pe, int[] in, int[] post) {
+		if (is > ie || ps > pe) return;
+		pre[idx++] = post[pe];
+		for (int i = is; i < N; i++) {
+			if (in[i] == post[pe]) {
+				tree(is, i - 1, ps, ps + i - is - 1, in, post);
+				tree(i + 1, ie, ps + i - is, pe - 1, in, post);
+			}
+		}
+	}
 }
